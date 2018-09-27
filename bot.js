923222f18bde787cb052423487f8b023123f58e9
Bolzon <blzn@mail.ru>
2018-09-27 08:55:51 -0300

    initial version for universal echo bot

---
+++ bot.js
@@ -0,0 +1,7 @@
+const builder = require('botbuilder')
+const connector = new builder.ConsoleConnector().listen()
+const bot = new builder.UniversalBot(connector)
+
+bot.dialog('/', session => {
+    session.send('hello world')
+})
