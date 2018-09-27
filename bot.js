afd4d889f3ba82e1e8e849d3f49ad14a8f8208a9
Bolzon <blzn@mail.ru>
2018-09-27 09:26:37 -0300

    inserting user prompt

--- bot.js
+++ bot.js
@@ -3,6 +3,18 @@ const builder = require('botbuilder');
 const connector = new builder.ConsoleConnector().listen();
 const bot = new builder.UniversalBot(connector);
 
+/*
 bot.dialog('/', session => {
     session.send('hello world');
 });
+*/
+
+bot.dialog('/', [
+    session => {
+            builder.Prompts.text(session, 'Hi, what is your name?');
+    },
+    (session, results) => {
+            console.log(results);
+            session.send('Hello %s', results.response);
+    }
+]);
